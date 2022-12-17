d12702f89f628065331d1e9eb36bedcaee52098a
dl10yr <tkzw04202166@gmail.com>
2022-12-17 22:54:38 +0900

    add  config

--- app/todo/ssr/page.tsx
+++ app/todo/ssr/page.tsx
@@ -1,6 +1,8 @@
 import { prisma } from '@/lib/server/db'
 
 export const revalidate = 0
+export const dynamic = 'force-dynamic'
+export const fetchCache = 'force-no-store'
 
 async function getTodos() {
   return await prisma.todo.findMany({
