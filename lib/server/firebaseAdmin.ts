4a3b33dc68524c382a08cda1d863c3d076b37894
dl10yr <tkzw04202166@gmail.com>
2024-11-24 21:19:56 +0900

    fix warning

--- lib/server/firebaseAdmin.ts
+++ lib/server/firebaseAdmin.ts
@@ -8,7 +8,7 @@ export const firebaseAdmin =
   apps[0] ??
   initializeApp({
     credential: admin.credential.cert({
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      privateKey: process.env.FIREBASE_PRIVATE_KEY ?? ''.replace(/\\n/g, '\n'),
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       projectId: process.env.FIREBASE_PROJECT_ID,
     }),
