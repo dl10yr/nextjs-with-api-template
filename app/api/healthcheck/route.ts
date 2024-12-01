2aaaf635b093532ff2cc3fc6df54b2dd5fbcdea6
dl10yr <tkzw04202166@gmail.com>
2024-12-01 09:48:00 +0900

    feat: 2024 healthcheckのexportを修正

--- app/api/healthcheck/route.ts
+++ app/api/healthcheck/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 
-export default function GET(req: Request, { params }) {
+export function GET(req: Request, { params }) {
   return NextResponse.json({
     helthcheck: 'ok',
   })
